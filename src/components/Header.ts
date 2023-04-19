a2d9253f7bf70c8e6da6264cd05aa556460f0a5e
regularPark <srk99279@naver.com>
2023-04-19 10:31:15 +0900

    test: inputmode 테스트

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -2,7 +2,7 @@ class Header {
   template = `
         <h1 class="logo"><img src="assets/logo.png" alt="MovieList 로고" /></h1>
         <form class="search-box">
-          <input type="text" name="search-text" placeholder="검색" />
+          <input inputmode="numeric" type="text" name="search-text" placeholder="검색" />
           <button class="search-button">검색</button>
         </form>`;
 
